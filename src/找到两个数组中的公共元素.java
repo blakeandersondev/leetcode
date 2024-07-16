7991e3b38a1385109d0e01b21cbf2ee2b6c661b0
blake <zhaoyunlong995@gmail.com>
2024-07-16 15:16:59 +0800

    remove note

--- src/找到两个数组中的公共元素.java
+++ src/找到两个数组中的公共元素.java
@@ -1,15 +1,5 @@
 public class 找到两个数组中的公共元素 {
 
-    /*
-    给你两个下标从 0 开始的整数数组 nums1 和 nums2 ，它们分别含有 n 和 m 个元素。
-
-    请你计算以下两个数值：
-
-    统计 0 <= i < n 中的下标 i ，满足 nums1[i] 在 nums2 中 至少 出现了一次。
-    统计 0 <= i < m 中的下标 i ，满足 nums2[i] 在 nums1 中 至少 出现了一次。
-    请你返回一个长度为 2 的整数数组 answer ，按顺序 分别为以上两个数值。
-     */
-
     public int[] findIntersectionValues(int[] nums1, int[] nums2) {
         int res1 = 0;
         int res2 = 0;
