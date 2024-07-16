ffa8ff072cd589dd1c4656b11c9824ac1024f4d6
blake <zhaoyunlong995@gmail.com>
2024-07-16 16:33:36 +0800

    移除元素

---
+++ src/problem/移除元素.java
@@ -0,0 +1,20 @@
+package problem;
+
+public class 移除元素 {
+
+    public int removeElement(int[] nums, int val) {
+        int left = 0;
+        int right = nums.length;
+
+        while (left < right) {
+            if (nums[left] == val) {
+                nums[left] = nums[right - 1];
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return left;
+    }
+
+}
