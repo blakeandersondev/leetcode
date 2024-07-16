b222fef3cb6ed4ece26fdc2787c911fcafc8f88a
blake <zhaoyunlong995@gmail.com>
2024-07-16 17:11:10 +0800

    删除有序数组中的重复项 提供第二种方法

--- src/problem/删除有序数组中的重复项.java
+++ src/problem/删除有序数组中的重复项.java
@@ -13,15 +13,17 @@ public class 删除有序数组中的重复项 {
         return idx;
     }
 
-    public static void main(String[] args) {
-        删除有序数组中的重复项 inst = new 删除有序数组中的重复项();
+    public int removeDuplicates2(int[] nums) {
+        int left = 1;
+        int right = 1;
 
-        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
-        int result = inst.removeDuplicates(nums);
-        System.out.println(result);
-        for (int n : nums) {
-            System.out.print(n + " ");
+        while (right < nums.length) {
+            if (nums[left - 1] != nums[right]) {
+                nums[left++] = nums[right];
+            } else {
+                right++;
+            }
         }
+        return left;
     }
-
 }
