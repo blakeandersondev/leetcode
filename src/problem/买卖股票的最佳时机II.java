ba37efc680019a59865b50db13db485656eace23
blake <zhaoyunlong995@gmail.com>
2024-07-17 22:10:37 +0800

    买卖股票的最佳时机II

---
+++ src/problem/买卖股票的最佳时机II.java
@@ -0,0 +1,28 @@
+package problem;
+
+public class 买卖股票的最佳时机II {
+
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+
+        return dp[n - 1][0];
+    }
+
+    public static void main(String[] args) {
+        买卖股票的最佳时机II inst = new 买卖股票的最佳时机II();
+
+        int[] prices = new int[]{7, 1, 5, 3, 6, 4};
+        int result = inst.maxProfit(prices);
+        System.out.println(result);
+    }
+
+}
